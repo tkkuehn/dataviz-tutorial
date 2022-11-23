b9c8e7cc26b951cf2a9dfa67eacf1877b8bd1cc1
Tristan Kuehn <tkuehn@uwo.ca>
2022-11-23 17:30:34 -0500

    Use a style sheet

--- line_plot.py
+++ line_plot.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 RNG = np.random.default_rng(1)
 
 
-def generate_data():
+def generate_data(num_sets):
     """Generate some random example data."""
     x_data = np.linspace(0, 2, 7)
-    y_data = RNG.uniform(-0.75, 0.5, 7)
+    y_data = RNG.uniform(-0.75, 0.5, (7, num_sets))
 
     return x_data, y_data
 
@@ -19,22 +19,14 @@ def basic_plot(x_data, y_data):
 
 
 def plot_custom_colour(ax, x_data, y_data):
-    line, = ax.plot(x_data, y_data, color="black")
-    line.set_linewidth(2)
-    return ax, line
+    (line,) = ax.plot(x_data, y_data, color="black")
+    return line
 
 
 def adjust_ticks(ax, x_ticks, y_ticks):
-    ax.tick_params(labelsize=8)
     ax.set_xticks(x_ticks)
     ax.set_yticks(y_ticks)
 
-    return ax
-
-def remove_x_ticks(ax):
-    ax.tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
-
-    return ax
 
 def add_labels(ax, x_label=None, y_label=None):
     if x_label:
@@ -42,12 +34,8 @@ def add_labels(ax, x_label=None, y_label=None):
     if y_label:
         ax.set_ylabel(y_label, size=8)
 
-    return ax
-
 
 def remove_box(ax, x_bounds=None, y_bounds=None):
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
     if x_bounds:
         ax.spines["bottom"].set_bounds(x_bounds)
     else:
@@ -56,25 +44,41 @@ def remove_box(ax, x_bounds=None, y_bounds=None):
         ax.spines["left"].set_bounds(y_bounds)
     else:
         ax.spines["left"].set_visible(False)
-    return ax
 
 
 def main():
-    my_data = generate_data()
-    width = 2.63 
-    fig, axes = plt.subplots(2, 1, figsize=(width, width / 1.5), dpi=300, layout="tight")
+    my_data = generate_data(2)
+    width = 2.63
+    plt.style.use("example.mplstyle")
+    fig, axes = plt.subplots(
+        2,
+        1,
+        figsize=(width, width / 1.5),
+        dpi=300,
+        layout="tight",
+        sharex=True,
+        sharey=True,
+    )
+
     ax = axes[0]
-    ax, _ = plot_custom_colour(ax, *my_data)
-    ax = adjust_ticks(ax, my_data[0][::2], [min(my_data[1]), 0, max(my_data[1])])
-    ax = remove_x_ticks(ax)
-    ax = remove_box(ax, y_bounds=(min(my_data[1]), max(my_data[1])))
-    ax = add_labels(ax, y_label="Series 1")
+    line_1 = plot_custom_colour(ax, my_data[0], my_data[1][:, 0])
+    adjust_ticks(ax, my_data[0][::2], [min(my_data[1][:, 0]), 0, max(my_data[1][:, 0])])
+    add_labels(ax, y_label="Series 1")
+    remove_box(
+        ax,
+        x_bounds=[min(my_data[0]), max(my_data[0])],
+        y_bounds=[min(my_data[1][:, 0]), max(my_data[1][:, 0])],
+    )
 
     ax = axes[1]
-    ax, _ = plot_custom_colour(ax, *my_data)
-    ax = adjust_ticks(ax, my_data[0][::2], [min(my_data[1]), 0, max(my_data[1])])
-    ax = add_labels(ax, x_label="Time (s)", y_label="Series 2")
-    ax = remove_box(ax, (min(my_data[0]), max(my_data[0])), (min(my_data[1]), max(my_data[1])))
+    line_2 = plot_custom_colour(ax, my_data[0], my_data[1][:, 1])
+    adjust_ticks(ax, my_data[0][::2], [min(my_data[1][:, 1]), 0, max(my_data[1][:, 1])])
+    add_labels(ax, x_label="Time (s)", y_label="Series 2")
+    remove_box(
+        ax,
+        x_bounds=[min(my_data[0]), max(my_data[0])],
+        y_bounds=[min(my_data[1][:, 1]), max(my_data[1][:, 1])],
+    )
 
     fig.supylabel("Voltage (V)", size=8)
 
